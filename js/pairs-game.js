6477c46824385cb9241d2bc2a76898315cb4ed34
Vladislav Surin <vlad_surin@mail.ru>
2023-01-13 19:22:57 +0300

    added button play again

--- js/pairs-game.js
+++ js/pairs-game.js
@@ -9,13 +9,30 @@ window.addEventListener('DOMContentLoaded', () => {
     return element;
   }
 
-  function addElementIntoParent(parent, element) {
+  function renderElement(parent, element) {
     parent.append(element);
   }
 
+  const sectionMain = createElement('section', 'main'),
+        container = createElement('div', 'container'),
+        title = createElement('h1', 'title', 'Игра в пары'),
+        game = createElement('ul', 'game'),
+        playAgainBtn = createElement('button', 'play-again-btn', 'Сыграть ещё раз');
+
+  playAgainBtn.addEventListener('click', () => {
+    startGame(8);
+    playAgainBtn.style.display = 'none';
+  });
+
+  renderElement(document.body, sectionMain);
+  renderElement(sectionMain, container);
+  renderElement(container, title);
+  renderElement(container, game);
+  renderElement(container, playAgainBtn);
+
   function createArrayOfNumbers(amount) {
     const arr = [];
-    for (let i = 1; i <= amount / 2; i++) {
+    for (let i = 1; i <= amount; i++) {
       arr.push(i, i);
     }
     return arr;
@@ -31,19 +48,23 @@ window.addEventListener('DOMContentLoaded', () => {
   function createAndInsertCards(arr, parent) {
     arr.forEach(numberOfCard => {
       const card = createElement('li', 'card', numberOfCard);
-      addElementIntoParent(parent, card);
+      renderElement(parent, card);
     });
   }
 
-  function startGame(amountCards, parent) {
-    const arrOfNumbers = createArrayOfNumbers(amountCards);
+  function startGame(amountPairs) {
+    game.replaceChildren();
+    const arrOfNumbers = createArrayOfNumbers(amountPairs);
     shuffleArray(arrOfNumbers);
-    createAndInsertCards(arrOfNumbers, parent);
+    createAndInsertCards(arrOfNumbers, game);
 
-    let firstCard = null,
-        secondCard = null;
+    const storage = {
+      firstCard: null,
+      secondCard: null,
+      openedCards: 0
+    };
 
-    parent.addEventListener('click', (e) => {
+    game.addEventListener('click', (e) => {
       const currenElement = e.target;
 
       if (!currenElement.classList.contains('card') ||
@@ -52,37 +73,33 @@ window.addEventListener('DOMContentLoaded', () => {
             return;
       }
 
-      if (secondCard && firstCard.textContent !== secondCard.textContent) {
-        firstCard.classList.remove('card--open');
-        secondCard.classList.remove('card--open');
-        firstCard = null;
-        secondCard = null;
+      if (storage.secondCard && storage.firstCard.textContent !== storage.secondCard.textContent) {
+        storage.firstCard.classList.remove('card--open');
+        storage.secondCard.classList.remove('card--open');
+        storage.firstCard = null;
+        storage.secondCard = null;
       }
 
-      if (!firstCard) {
-        firstCard = currenElement;
-        firstCard.classList.add('card--open');
-      } else if (firstCard.textContent !== currenElement.textContent) {
-        secondCard = currenElement;
-        secondCard.classList.add('card--open');
+      if (!storage.firstCard) {
+        storage.firstCard = currenElement;
+        storage.firstCard.classList.add('card--open');
+      } else if (storage.firstCard.textContent !== currenElement.textContent) {
+        storage.secondCard = currenElement;
+        storage.secondCard.classList.add('card--open');
       } else {
-        firstCard.classList.add('card--success');
+        storage.firstCard.classList.add('card--success');
         currenElement.classList.add('card--success');
-        firstCard = null;
-        secondCard = null;
+        storage.firstCard = null;
+        storage.secondCard = null;
+        storage.openedCards++;
+
+        if(storage.openedCards === amountPairs) {
+          playAgainBtn.style.display = 'block';
+          storage.openedCards = 0;
+        }
       }
     });
   }
 
-  const sectionMain = createElement('section', 'main'),
-        container = createElement('div', 'container'),
-        title = createElement('h1', 'title', 'Пары'),
-        game = createElement('ul', 'game');
-
-  addElementIntoParent(document.body, sectionMain);
-  addElementIntoParent(sectionMain, container);
-  addElementIntoParent(container, title);
-  addElementIntoParent(container, game);
-
-  startGame(16, game);
+  startGame(8);
 });
